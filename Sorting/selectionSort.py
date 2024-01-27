5fe275b581de209c1708457c162a2974a1381182
Bharathwaj <imbharathwaj@gmail.com>
2024-01-27 10:35:15 +0530

    Comprehensible comment added

--- Sorting/selectionSort.py
+++ Sorting/selectionSort.py
@@ -2,18 +2,27 @@ def selectionSort(arr):
     n = len(arr)
     for i in range(n):
         min = i
-        for j in range(n):
-            if (arr[j] > arr[min]):
+
+        # Range of inner loop should be starts after
+        # sorted part of array
+        for j in range(i + 1, n):
+            if arr[j] < arr[min]:
                 min = j
-    # if min != i:
-    temp = arr[i]
-    arr[i] = arr[min]
-    arr[min] = temp
-    
-    for d in range(n):
-        print(arr[d])
+
+        # # Swapping
+        temp = arr[i]
+        arr[i] = arr[min]
+        arr[min] = temp
 
 
-if __name__ == '__main__':
-    arr=[7,4,10,8,3,1]
+def printArr(arr):
+    for a in arr:
+        print(a, end=" ")
+    print()
+
+
+if __name__ == "__main__":
+    arr = [7, 4, 10, 8, 3, 1]
+    printArr(arr)
     selectionSort(arr)
+    printArr(arr)
