4b64a97dc178feac937ccaf41bf6baf6906c148b
Bharathwaj <imbharathwaj@gmail.com>
2024-01-30 11:15:03 +0530

    Ternary Search Iterative approach
    Ternary Search Iterative approach
    Time complexity: O( log 3n)

---
+++ Search/TernarySearch_Iterative.py
@@ -0,0 +1,26 @@
+def ternarySearch(arr, key, lb, ub):
+    while ub >= lb:
+        mid1 = lb + (ub - lb) // 3
+        mid2 = ub - (ub - lb) // 3
+
+        if key == arr[mid1]:
+            return mid1
+        if key == arr[mid2]:
+            return mid2
+
+        if key < arr[mid1]:
+            ub = mid1 - 1
+        elif key > arr[mid2]:
+            lb = mid2 + 1
+        else:
+            lb = mid1 + 1
+            ub = mid2 - 1
+    return -1
+
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    key = 6
+    lb = 0
+    ub = len(arr)
+    print(ternarySearch(arr, key, lb, ub))
